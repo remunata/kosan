c7b18c532c2fa71a89d79043fb978cbef632ff03
Mahendra Dinata <remdinata@gmail.com>
2023-11-27 11:46:24 +0700

    finishing footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,63 @@
 import edasLogo from "../assets/edas-logo.png";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faHouse } from "@fortawesome/free-solid-svg-icons";
+
 function Footer() {
   return (
     <>
-      <div className="flex justify-between items-start px-40 w-full bg-slate-700 z-50 pb-20 pt-10">
-        <div className="flex flex-col items-center">
-          <img src={edasLogo} alt="logo" className="h-72" />
-          <h1 className="text-5xl font-bold text-white">KOSAN</h1>
+      <div className="px-40 w-full bg-slate-700 z-50 pb-20 pt-10 text-white flex flex-col relative">
+        <div className="w-full flex justify-between items-center">
+          <span className="text-md italic">
+            This application is open source, you can contribute in GitHub
+          </span>
+          <a href="https://github.com/remunata/kosan" target="_blank">
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="text-3xl cursor-pointer"
+            />
+          </a>
         </div>
-        <div className="mt-20">
-          <h1 className="text-2xl font-bold text-white">Navigation</h1>
-        </div>
-        <div className="mt-20">
-          <h1 className="text-2xl font-bold text-white">Universitas Sriwijaya</h1>
+        <hr className="mt-6 mb-8" />
+        <div className="flex justify-between items-start gap-20">
+          <div className="flex flex-col">
+            <div className="flex items-center">
+              <img src={edasLogo} alt="edas-logo" className="h-16" />
+              <h1 className="text-3xl font-bold">KOSAN</h1>
+            </div>
+            <div>
+              <p className="mt-6">
+                Solusi untuk menemukan tempat tinggal anda di sekitar kampus
+                <br />
+                Universitas Sriwijaya Indralaya dengan menggunakan metode
+                <br />
+                EDAS (Evaluation based on Distance from Average Solution)
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-3xl font-bold pt-2">AUTHORS</h1>
+            <div className="mt-8 flex flex-col gap-4">
+              <span>Dellin Irawan</span>
+              <span>Hanif Syahri Ramadhani</span>
+              <span>Mahendra Dinata</span>
+              <span>Risky Armansyah</span>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-3xl font-bold pt-2">CONTACT</h1>
+            <div className="mt-8 flex flex-col gap-4">
+              <span>
+                <FontAwesomeIcon icon={faHouse} className="mr-2" />
+                Fakultas Ilmu Komputer, Universitas Sriwijaya
+              </span>
+              <span>
+                <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
+                remdinata@gmail.com
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,11 @@ function Header() {
       <div className="flex justify-between items-center px-10 w-full h-14 bg-slate-700 z-50">
         <div className="flex items-center">
           <img src={edasLogo} alt="logo" className="h-12" />
-          <h1 className="text-2xl font-bold text-white">KOSAN</h1>
+          <a href="/" className="text-2xl font-bold text-white">KOSAN</a>
         </div>
         <a
           href="#calculation"
-          className="px-3 py-1 rounded-sm bg-white font-medium"
+          className="px-3 py-1 rounded-sm bg-white font-medium hover:bg-gray-200 transition-colors"
         >
           Get Started
         </a>
